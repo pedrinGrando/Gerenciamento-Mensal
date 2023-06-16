a5c900a6fb562b0235f2db682b24bdb74dc74fcf
Pedro Aluisio Scuissiatto <121454190+pedrinGrando@users.noreply.github.com>
2023-06-16 16:53:41 -0300

    Update PainelConsultarDespesa.java

--- src/main/java/model/view/PainelConsultarDespesa.java
+++ src/main/java/model/view/PainelConsultarDespesa.java
@@ -5,6 +5,8 @@ import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -23,6 +25,8 @@ import javax.swing.border.EmptyBorder;
 
 import model.vo.*;
 import controller.*;
+import exceptions.CampoInvalidoException;
+
 import javax.swing.JTextField;
 import javax.swing.border.SoftBevelBorder;
 import javax.swing.border.BevelBorder;
@@ -32,27 +36,26 @@ import javax.swing.border.MatteBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.border.EtchedBorder;
 
-
 public class PainelConsultarDespesa extends JPanel {
 	private JComponent btnNewButton_1;
 	
 	DespesaVO despesaVO = new DespesaVO();
 	DespesaController despController = new DespesaController();
 	private JTextField campDespDigitada;
-	private JLabel lblNewLabel;
+	private JLabel visaoNome;
 	DecimalFormat formato = new DecimalFormat("#,##0.00");
 
-	private JLabel lblNewLabel_6;
+	private JLabel lblTitulo;
 
-	private JLabel lblNewLabel_1;
+	private JLabel lblNomeConsult;
 
 	private JLabel lblNome;
 
-	private JLabel lblNewLabel_3;
+	private JLabel lblDespConsult;
 
 	private JLabel lblDespesa;
 
-	private JLabel lblNewLabel_5;
+	private JLabel lblValorConsult;
 
 	private JLabel lblValor;
 
@@ -68,52 +71,64 @@ public class PainelConsultarDespesa extends JPanel {
 		setBackground(new Color(0, 255, 255));
 		setLayout(null);
 		
-		lblNewLabel_6 = new JLabel(" Consultar de despesa ");
-		lblNewLabel_6.setFont(new Font("Source Serif Pro Semibold", Font.BOLD | Font.ITALIC, 16));
-		lblNewLabel_6.setBounds(168, 24, 183, 23);
-		add(lblNewLabel_6);
-		
+		lblTitulo = new JLabel(" Consultar de despesa ");
+		lblTitulo.setFont(new Font("Source Serif Pro Semibold", Font.BOLD | Font.ITALIC, 16));
+		lblTitulo.setBounds(168, 24, 183, 23);
+		add(lblTitulo);
 		
 		campDespDigitada = new JTextField();
-		campDespDigitada.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
-		campDespDigitada.setBounds(135, 88, 296, 20);
-		add(campDespDigitada);
+		campDespDigitada.setBorder(null);
+		campDespDigitada.setBounds(135, 88, 173, 20);
 		campDespDigitada.setColumns(10);
 		
-		lblNewLabel = new JLabel("Nome da despesa :");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
-		lblNewLabel.setBounds(20, 91, 115, 14);
-		add(lblNewLabel);
+		// Adiciona um ouvinte de eventos de teclado ao campo
+		campDespDigitada.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                
+                // Verifica se o caractere é um número
+                if (Character.isDigit(c)) {
+                    e.consume(); // Impede que o caractere seja inserido
+                }
+            }
+        });
+		add(campDespDigitada);
+		
+		visaoNome = new JLabel("Nome da despesa :");
+		visaoNome.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
+		visaoNome.setBounds(20, 91, 115, 14);
+		add(visaoNome);
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(0, 0, 0));
 		panel.setBounds(0, 145, 700, 14);
 		add(panel);
 		
-		lblNewLabel_1 = new JLabel("Nome : ");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
-		lblNewLabel_1.setBounds(20, 203, 71, 14);
-		add(lblNewLabel_1);
+		lblNomeConsult = new JLabel("Nome : ");
+		lblNomeConsult.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
+		lblNomeConsult.setBounds(20, 203, 71, 14);
+		add(lblNomeConsult);
 		
 		lblNome = new JLabel("");
 		lblNome.setFont(new Font("Tahoma", Font.ITALIC, 11));
 		lblNome.setBounds(73, 203, 209, 14);
 		add(lblNome);
 		
-		lblNewLabel_3 = new JLabel("Despesa : ");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
-		lblNewLabel_3.setBounds(20, 247, 71, 14);
-		add(lblNewLabel_3);
+		lblDespConsult = new JLabel("Despesa : ");
+		lblDespConsult.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
+		lblDespConsult.setBounds(20, 247, 71, 14);
+		add(lblDespConsult);
 		
 		lblDespesa = new JLabel("");
 		lblDespesa.setFont(new Font("Tahoma", Font.ITALIC, 11));
 		lblDespesa.setBounds(87, 247, 250, 14);
 		add(lblDespesa);
 		
-		lblNewLabel_5 = new JLabel("Valor : ");
-		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
-		lblNewLabel_5.setBounds(20, 288, 71, 14);
-		add(lblNewLabel_5);
+		lblValorConsult = new JLabel("Valor : ");
+		lblValorConsult.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
+		lblValorConsult.setBounds(20, 288, 71, 14);
+		add(lblValorConsult);
 		
 		lblValor = new JLabel("");
 		lblValor.setFont(new Font("Tahoma", Font.ITALIC, 11));
@@ -122,35 +137,43 @@ public class PainelConsultarDespesa extends JPanel {
 		
 		btnConsultar = new JButton("");
 		btnConsultar.setBorder(null);
-		btnConsultar.setBackground(new Color(0, 255, 255));
+		btnConsultar.setBackground(new Color(255, 255, 255));
 		btnConsultar.setIcon(new ImageIcon(PainelConsultarDespesa.class.getResource("/icons/loupe.png")));
 		btnConsultar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				if (campDespDigitada.getText().isBlank() || campDespDigitada.getText().isEmpty()) {
-					
-					JOptionPane.showMessageDialog(null, "O nome é obrigratório", "Gerenciamento-Mensal", JOptionPane.WARNING_MESSAGE);
-					
-				} else {
+				String despNome = campDespDigitada.getText();
+				
+				try {
 					despesaVO = despController.consultarDespesaController(campDespDigitada.getText(), userOnline);
 					
-					lblNome.setText(userOnline.getNome());
+					if (despesaVO.getIdDespesa() == 0) {
+						
+						JOptionPane.showMessageDialog(null, "Despesa " + despNome+ " \nnão encontrada!", "", JOptionPane.ERROR_MESSAGE);
+						
+					} else {
+					
+                    lblNome.setText(userOnline.getNome());
 					
 					lblDespesa.setText(despesaVO.getDespNome());
 					
 					lblValor.setText("R$ "+ formato.format(despesaVO.getValor()));
+					}
+                  
+				} catch (CampoInvalidoException e1) {
 					
+					JOptionPane.showMessageDialog(null, "Despesa não encontrada!"
+							, "GS - Gerenciador de salário", JOptionPane.WARNING_MESSAGE);
 				}
 				
 			}
 		});
-		btnConsultar.setBounds(429, 85, 39, 23);
+		btnConsultar.setBounds(303, 88, 38, 20);
 		add(btnConsultar);
 		
 		JLabel lblNewLabel_2 = new JLabel("");
 		lblNewLabel_2.setIcon(new ImageIcon(PainelConsultarDespesa.class.getResource("/icons/bank.png")));
 		lblNewLabel_2.setBounds(620, 452, 32, 32);
 		add(lblNewLabel_2);
-		
 	}
 }
