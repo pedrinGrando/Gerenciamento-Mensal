65a614c15038a7e09baff6475d3d18d8c9f82c6c
Pedro Aluisio Scuissiatto <121454190+pedrinGrando@users.noreply.github.com>
2023-06-16 16:52:58 -0300

    Update PainelRemoverDespesa.java

--- src/main/java/model/view/PainelRemoverDespesa.java
+++ src/main/java/model/view/PainelRemoverDespesa.java
@@ -5,6 +5,8 @@ import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -23,13 +25,12 @@ import model.vo.*;
 import javax.swing.border.SoftBevelBorder;
 import javax.swing.border.BevelBorder;
 
-
 public class PainelRemoverDespesa extends JPanel {
 
 	private JTextField nomeDespesa_camp;
-	private JLabel lblNewLabel_1;
+	private JLabel visaoNome;
 	private JButton btn_removerDespesa;
-	private JLabel lblNewLabel_2;
+	private JLabel visaoTitulo;
 	private JButton btn_voltar;
 	
 	DespesaController despesaController = new DespesaController();
@@ -47,13 +48,25 @@ public class PainelRemoverDespesa extends JPanel {
 		nomeDespesa_camp = new JTextField();
 		nomeDespesa_camp.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
 		nomeDespesa_camp.setBounds(42, 94, 181, 20);
-		add(nomeDespesa_camp);
 		nomeDespesa_camp.setColumns(10);
 		
-		lblNewLabel_1 = new JLabel("Nome da despesa");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		lblNewLabel_1.setBounds(32, 77, 101, 14);
-		add(lblNewLabel_1);
+		nomeDespesa_camp.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                
+                // Verifica se o caractere é um número
+                if (Character.isDigit(c)) {
+                    e.consume(); // Impede que o caractere seja inserido
+                }
+            }
+        });
+		add(nomeDespesa_camp);
+		
+		visaoNome = new JLabel("Nome da despesa");
+		visaoNome.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		visaoNome.setBounds(32, 77, 101, 14);
+		add(visaoNome);
 		
 		btn_removerDespesa = new JButton("");
 		btn_removerDespesa.setIcon(new ImageIcon(PainelRemoverDespesa.class.getResource("/icons/remove.png")));
@@ -75,21 +88,20 @@ public class PainelRemoverDespesa extends JPanel {
 		btn_removerDespesa.setBounds(222, 94, 41, 23);
 		add(btn_removerDespesa);
 		
-		lblNewLabel_2 = new JLabel(" Remover despesa cadastrada ");
-		lblNewLabel_2.setFont(new Font("Source Serif Pro Semibold", Font.BOLD | Font.ITALIC, 14));
-		lblNewLabel_2.setBounds(88, 34, 279, 14);
-		add(lblNewLabel_2);
+		visaoTitulo = new JLabel(" Remover despesa cadastrada ");
+		visaoTitulo.setFont(new Font("Source Serif Pro Semibold", Font.BOLD | Font.ITALIC, 14));
+		visaoTitulo.setBounds(88, 34, 279, 14);
+		add(visaoTitulo);
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(0, 0, 0));
 		panel.setBounds(0, 195, 739, 10);
 		add(panel);
 		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(PainelRemoverDespesa.class.getResource("/icons/bank.png")));
-		lblNewLabel.setBounds(636, 430, 41, 34);
-		add(lblNewLabel);
-		
+		JLabel lblIcon = new JLabel("");
+		lblIcon.setIcon(new ImageIcon(PainelRemoverDespesa.class.getResource("/icons/bank.png")));
+		lblIcon.setBounds(636, 430, 41, 34);
+		add(lblIcon);
     }
 }
 
